714c5f85d4f958cbc726492629a23c89696fccc0
anterou <anterou76@gmail.com>
2023-09-23 18:58:00 +0300

    Made a LeetCode exercise

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import time,webbrowser
-def validator(func):
-    def wrapper(url):
-        if "." in url:
-            time.sleep(2)
-            func(url)
-        else:
-            print("Иди в очко")
-    return wrapper
-@validator
-def open_url(url):
-    webbrowser.open(url)
-open_url("https://vk.com")
+class Solution:
+    def twoSum(self, nums, target):
+        var = {}
+        for i, value in enumerate(nums):
+            remaining = target - nums[i]
+            if remaining in var:
+                return (i, var[remaining])
+            else:
+                var[value] = i
