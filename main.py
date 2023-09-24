d6b4138e15d6336492485958436955aa1673510f
anterou <anterou76@gmail.com>
2023-09-24 19:13:34 +0300

    My matrix NumPy app

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-class Solution:
-    def twoSum(self, nums, target):
-        var = {}
-        for i, value in enumerate(nums):
-            remaining = target - nums[i]
-            if remaining in var:
-                return (i, var[remaining])
-            else:
-                var[value] = i
+import numpy as np
+
+# Ask the user for the number of elements
+num_elements = int(input("Enter the number of elements: "))
+# Calculate the matrix dimension as the nearest square root of the number of elements
+matrix_size = int(num_elements ** 0.5)
+# Determine the number of rows and columns automatically
+# If the matrix is square (fully populated), keep its dimension as matrix_size
+# Otherwise, the number of rows will be matrix_size, and the number of columns will be matrix_size + 1
+if matrix_size ** 2 == num_elements:
+    rows = cols = matrix_size
+else:
+    rows = matrix_size
+    cols = matrix_size + 1
+# Create a matrix using arange
+matrix = np.arange(rows * cols).reshape(rows, cols)
+print("Matrix:")
+print(matrix)
+
+
+
+
