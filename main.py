51d00ddf0151e94b818f11329661efc720fec6c6
anterou <anterou76@gmail.com>
2023-10-25 17:40:43 +0300

    My second recursive function which finds length of list

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def sum(arr):
+
+def length(arr):
+    count =0
     if len(arr)==0:
         return 0
-    elif len(arr)==1:
-        return arr[0]
     else:
-        a = arr[0]
-        arr.pop(0)
-        return a+sum(arr)
-print(sum([5, 4, 3, 2, 10]))
+        return 1+length(arr[1:])
+print(length([5, 4, 3, 2, 10]))
