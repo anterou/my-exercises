6e4d5969531a266592cda8075b5105ca56dc0706
anterou <anterou76@gmail.com>
2023-10-29 13:14:55 +0300

    It is something easier for sum but its experience too

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-def binary_search(list, item, low=0, high=None):
-    if high == None:
-        high = len(list)-1
-    if low>high:
-        return None
+def sum(arr):
+    if len(arr)==0:
+        return 0
+    elif len(arr)==1:
+        return arr[0]
     else:
-        mid = (low+high)//2
-        if list[mid] == item:
-            return mid
-        if list[mid] > item:
-            return binary_search(list, item, low, mid-1)
-        else:
-            return binary_search(list, item, mid+1, high)
-print(binary_search([1,2,3,5,12], 3))
-
+        return arr[0]+sum(arr[1:])
+a = sum([1,2,3])
+print(a)
